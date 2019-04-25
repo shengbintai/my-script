5b02fb3b54954ad48aff21b7195c8eb36c25652b
shengbintai <xiaotai-2666@163.com>
2019-04-25 14:00:02 +0800

    修改vivado_prj_template.sh
    增加Work_Dir下的忽略文件及创建vivado tcl模板

--- vivado_prj_template.sh
+++ vivado_prj_template.sh
@@ -9,3 +9,18 @@ mkdir DSP
 mkdir Script
 mkdir Doc
 
+echo -e "*
+!run_vivado.tcl">Work_Dir/.gitignore
+
+echo -e "create_project pl2ps_prj ../Work_Dir/matlab_ctrl_pl -part xczu9eg-ffvb1156-2-i-es2 -force
+set_property board_part xilinx.com:zcu102_es2:part0:2.3 [current_project]
+add_files -fileset constrs_1 -norecurse ../Constraints/top.xdc
+current_project pl2ps_prj
+set_property  ip_repo_paths  ../IP [current_project]
+update_ip_catalog
+source ../IPI_BDs/system_top.tcl
+make_wrapper -files [get_files ../Work_Dir/matlab_ctrl_pl/pl2ps_prj.srcs/sources_1/bd/system_top/system_top.bd] -top
+add_files -norecurse ../Work_Dir/matlab_ctrl_pl/pl2ps_prj.srcs/sources_1/bd/system_top/hdl/system_top_wrapper.v
+update_compile_order -fileset sources_1
+start_gui
+" >Work_Dir/run_vivado.tcl
